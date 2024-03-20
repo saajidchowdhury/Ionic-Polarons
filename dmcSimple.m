0bce956018b7f6685236713274083ee13cb0479f
saajidchowdhury <61604839+saajidchowdhury@users.noreply.github.com>
2024-03-20 10:07:52 -0400

    Update dmcSimple.m

--- dmcSimple.m
+++ dmcSimple.m
@@ -6,16 +6,9 @@ n = 3;
 N0 = 500;
 t0 = 50000;
 dt = 100;
-C4 = 82.0563;
-C8 = 80157;
-b = -1;
-c = -1;
-C6 = 1.39339*10^3;
-C12 = 3.1925*10^8;
-f = 0; %Hz
 
 Nmax = 4*N0; 
-steps = 2*floor(t0/dt);
+steps = 2*t0/dt;
 R = 10;
 
 ER = 0;
@@ -46,9 +39,6 @@ for t = 1:steps
     for i = 1:Nmax
         if flag(i) ~= 0
             flag(i) = 1;
-            %1amu = 1822.89 electron mass
-            %mass of Li7 = 7.0160034366(45) amu
-            %mass of Yb174 = 173.938859 amu
             mLi = 7.0160034366 * 1822.89;
             mYb = 173.938859 * 1822.89;
             for j = 1:3
@@ -63,7 +53,7 @@ for t = 1:steps
     Vsum = 0;
     for i = 1:Nmax
         if flag(i) == 1
-            W = exp(-(V(x(i,:),C8,C12,b,c,f)-ER)*dt);
+            W = exp(-(V(x(i,:))-ER)*dt);
             mn = min(floor(W+rand),3);
             if mn == 0
                 flag(i) = 0;
@@ -81,7 +71,7 @@ for t = 1:steps
                 x(top2,:) = x(i,:);
             end
             N1 = N1 + mn;
-            Vsum = Vsum + mn*V(x(i,:),C8,C12,b,c,f);
+            Vsum = Vsum + mn*V(x(i,:));
         end
     end
     V1 = Vsum/N1;
@@ -108,41 +98,21 @@ ylabel("Reference Energy, $E_R$ (Hartree)", "FontSize", 20);
 ax = gca; ax.FontSize = 20;
 saveas(gcf, main+"/energy.png");
 
-
-
-
-function V = V(x, C8, C12, b, c, f)
+%potential
+function V = V(x)
     n = length(x)/3;
     V = 0;
-    C4 = 82.0563;
+    C4 = 82.0563; C8 = 80157;
     for j = 1:n-1
         R = sqrt((x(3*j+1)-x(1))^2+(x(3*j+2)-x(2))^2+(x(3*j+3)-x(3))^2);
-        if C8 == -1
-            V = V - C4*(R^2-c^2)/(R^2+c^2)*1/(b^2+R^2)^2;
-        elseif b == -1 && c == -1
-            V = V - C4/R^4 + C8/R^8;
-        else
-            fprintf("Error -1.\n");
-        end
+        V = V - C4/R^4 + C8/R^8;
     end
-    C6 = 1.39339*10^3;
+    C6 = 1.39339*10^3; C12 = 3.1925*10^8;
     for ja = 1:n-2
         for jb = ja+1:n-1
             R = sqrt((x(3*ja+1)-x(3*jb+1))^2 + (x(3*ja+2)-x(3*jb+2))^2 ...
                    + (x(3*ja+3)-x(3*jb+3))^2);
-            if C12 == -1
-                if R < 30
-                    V = V + 0.015;
-                end
-            else
-                V = V - C6/R^6 + C12/R^12;
-            end
+            V = V - C6/R^6 + C12/R^12;
         end
     end
-
-    %trap
-    m = 173.938859 * 1822.89;
-    w = 2*pi*f * 2.4188843265857*10^(-17);
-    R = sqrt(x(1)^2+x(2)^2+x(3)^2);
-    V = V + 0.5*m*w^2*R^2;
 end
